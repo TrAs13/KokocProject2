71b055268257dd074beca448d8c91fe88ebf9d08
Максим Волков <maksik-maksik-2001@mail.ru>
2022-06-17 20:36:40 +0300

    Работаю над блоком main

--- src/js/mainSlider.js
+++ src/js/mainSlider.js
@@ -1,27 +1,6 @@
-function initSlider(){
-$(".responsive").slick({
-  infinite: true,
-  dots: true,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
-}
-$(document).on('ready', function () {
-    initSlider();
+$(function () {
+  $(".slider").slick({
+    dots: true,
+    slidesToShow: 1,
+  });
 });
